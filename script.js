c9dbfcc100dfb35aab06671c705d68c6ee44d7ea
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 02:50:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// take input for the first variable and multiply by 2
+let firstVar = prompt("Enter the first variable:");
+let result = Number(firstVar) * 2;
+
+// take input for the second variable and add to the result
+let secondVar = prompt("Enter the second variable:");
+result += Number(secondVar);
+
+// display the final result
+alert("The final result is: " + result);
